9f334ce5d73771d9fcf6036035796b533d0edb8b
Tommy <SUP3RS4IY4NX@gmail.com>
2026-03-28 13:25:59 -0400

    Added concurrent request handling

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,32 @@
-use std::fmt::Debug;
-use std::fs::{self, File};
+use std::fs::File;
 use std::io::{BufReader, prelude::*};
 use std::net::{TcpListener, TcpStream};
-use std::ops::Index;
+use std::sync::{Arc, Mutex, mpsc::channel};
+use std::thread;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    let (tx, rx) = channel::<TcpStream>();
+    let reciever = Arc::new(Mutex::new(rx));
+
+    for _ in 0..4 {
+        let reciever = reciever.clone();
+        thread::spawn(move || {
+            loop {
+                let stream = reciever.lock().unwrap().recv().unwrap();
+                handle_connection(stream);
+            }
+        });
+    }
 
     for stream in listener.incoming() {
         let stream = stream.unwrap();
-        handle_connection(stream);
+        tx.send(stream).unwrap();
     }
 }
 
 fn handle_connection(mut stream: TcpStream) {
+    std::thread::sleep(std::time::Duration::from_secs(3));
     let reader = BufReader::new(&stream);
     let request_header = reader.lines().next().unwrap().unwrap();
 
